a180d0bcb143eaf5d4924c2786c4ed20a3e9744d
Sampreeta11 <Sampreeta11@github.com>
2026-07-02 18:04:29 +0530

    Fix Prisma edge bundler error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['172.18.240.1', 'localhost'],
+  serverExternalPackages: ['@prisma/client', 'bcrypt'],
 };
 
 export default nextConfig;
